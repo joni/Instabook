23207eb2b869a54007ad972610c947e56256c828
Joni Salonen <joni.salonen@gmail.com>
2012-04-28 13:37:43 +0200

    search screen

--- yii-app/protected/controllers/BookController.php
+++ yii-app/protected/controllers/BookController.php
@@ -2,45 +2,55 @@
 
 class BookController extends Controller
 {
-	public function actionIndex()
-	{
-		$this->render('index');
-	}
+    public function actionIndex()
+    {
+        $this->render('index');
+    }
 
-	public function actionSearch()
-	{
-		$this->render('search');
-	}
+    public function actionSearch()
+    {
+        $mdl = new BookSearchForm;
+        $results = array();
+        if (!empty($_POST['BookSearchForm']['query'])) {
+            $mdl->query = $_POST['BookSearchForm']['query'];
+            $results = $mdl->search();
+        }
 
-	public function actionView()
-	{
-		$this->render('view');
-	}
+        $this->render('search', array(
+            'model'=>$mdl, 
+            'books'=>$results,
+        ));
+    }
 
-	// Uncomment the following methods and override them if needed
-	/*
-	public function filters()
-	{
-		// return the filter configuration for this controller, e.g.:
-		return array(
-			'inlineFilterName',
-			array(
-				'class'=>'path.to.FilterClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
+    public function actionView()
+    {
+        $this->render('view');
+    }
 
-	public function actions()
-	{
-		// return external action classes, e.g.:
-		return array(
-			'action1'=>'path.to.ActionClass',
-			'action2'=>array(
-				'class'=>'path.to.AnotherActionClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
-	*/
+    // Uncomment the following methods and override them if needed
+        /*
+        public function filters()
+        {
+                // return the filter configuration for this controller, e.g.:
+                return array(
+                        'inlineFilterName',
+                        array(
+                                'class'=>'path.to.FilterClass',
+                                'propertyName'=>'propertyValue',
+                        ),
+                );
+        }
+
+        public function actions()
+        {
+                // return external action classes, e.g.:
+                return array(
+                        'action1'=>'path.to.ActionClass',
+                        'action2'=>array(
+                                'class'=>'path.to.AnotherActionClass',
+                                'propertyName'=>'propertyValue',
+                        ),
+                );
+        }
+         */
 }
