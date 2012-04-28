21adaa55764184a5697d3c72813061cb45df10ec
Joni Salonen <joni.salonen@gmail.com>
2012-04-28 17:31:48 +0200

    fixed voting

--- yii-app/protected/views/book/view.php
+++ yii-app/protected/views/book/view.php
@@ -7,11 +7,13 @@ $this->breadcrumbs=array(
 <div style="float: left; margin: 10px 10px 0 0">
 <?= CHtml::image($book->image) ?>
 </div>
-<h1><?= $book->title ?>
-<?= CHtml::link('+1',array('vote','id'=>$book->google_id,'vote'=>+1)) ?> / 
-<?= CHtml::link('-1',array('vote','id'=>$book->google_id,'vote'=>-1)) ?>
+<h1><?= $book->title ?> (<span style="color:green">+<?=$book->vote?></span> / <span style="color:red">-<?=$book->n_vote?></span>)
 </h1>
 <h2>By <?=$book->author ?></h2>
 
 <p><?= nl2br($book->description) ?></p>
+<span style="color: green"><?= CHtml::link('+1',array('vote','id'=>$book->google_id,'vote'=>+1)) ?> </span> /
+<span style="color: red">
+<?= CHtml::link('-1',array('vote','id'=>$book->google_id,'vote'=>-1)) ?></a>
+</span>
 </div>
