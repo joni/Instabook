214886011abbf321228faf95b234c30e7431acd8
Daniel González <daniel.gonzalez@freelancemadrid.es>
2012-04-28 17:45:02 +0200

    refs facebook social plugin

--- yii-app/protected/views/book/view.php
+++ yii-app/protected/views/book/view.php
@@ -15,3 +15,15 @@ $this->breadcrumbs=array(
 
 <p><?= nl2br($book->description) ?></p>
 </div>
+
+<div class="fb-comments" data-href="<?php echo Yii::app()->request->baseUrl; ?>" data-num-posts="10" data-width="910"></div>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1&appId=140209332712403";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>    
